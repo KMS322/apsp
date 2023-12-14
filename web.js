10e6ba1993f111c71afedd8a5ab05a3b6d6d427c
KMS322 <kms930322@naver.com>
2023-12-14 16:14:26 +0900

    231214_02

--- web.js
+++ web.js
@@ -12,5 +12,5 @@ app.get("/*", function (req, res) {
 const PORT = process.env.PORT || 8001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is run`);
 });
