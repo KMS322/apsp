b5f8a3881266e24667a379c66866dbc3a3c76364
KMS322 <kms930322@naver.com>
2023-12-14 16:10:16 +0900

    231214_01

---
+++ web.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+  console.log("get");
+});
+
+const PORT = process.env.PORT || 8001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${port}`);
+});
